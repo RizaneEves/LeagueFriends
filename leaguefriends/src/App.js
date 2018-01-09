7c9b0c2385e3f1bf43f3f96dd4ae23ede86e1104
Sheel Parekh <sdparek2@illinois.edu>
2018-01-09 15:31:01 -0500

    Add search functionality

--- leaguefriends/src/App.js
+++ leaguefriends/src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ChampList from './ChampList';
+import SearchBox from './SearchBox';
 import { champions } from './champions';
 
 
-const App = () => {
-	return (
-		<div>
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			champions: champions,
+			searchfield: ''
+		};
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value});
+	}
+
+	render() {
+
+		const filteredChampions = Object.keys(this.state.champions)
+			.filter(champion => champions[champion]['name'].toLowerCase().includes(this.state.searchfield.toLowerCase()))
+			.reduce((obj, champion) => {
+				obj[champion] = champions[champion];
+				return obj;
+			}, {});
+
+		return (
+		<div className = "tc">
 			<h1>LeagueFriends</h1>
-			<ChampList champions={champions}/>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<ChampList champions={filteredChampions}/>
 		</div>
 	);
+	}	
 }
 
 export default App;
